48d3efab29ddf62667bc1833e70eb89c674e7531
csae8092 <peter.andorfer@oeaw.ac.at>
2024-09-10 07:14:38 +0200

    replaces pkg_resources with importlib_resources; linting

--- tests/test_file_formats.py
+++ tests/test_file_formats.py
@@ -1,8 +1,8 @@
 import unittest
 from AcdhArcheAssets.file_formats import get_formats, get_by_mtype, get_by_extension
 
-MTYPE = 'image/png'
-EXTENSION = 'png'
+MTYPE = "image/png"
+EXTENSION = "png"
 
 
 class TestNormalizer(unittest.TestCase):
@@ -13,9 +13,9 @@ class TestNormalizer(unittest.TestCase):
     def test_002_get_by_mtype(self):
         items = get_by_mtype(MTYPE)
         self.assertEqual(type(items), list, "should be list")
-        self.assertTrue(EXTENSION in items[0]['extensions'])
+        self.assertTrue(EXTENSION in items[0]["extensions"])
 
     def test_003_get_by_extension(self):
         items = get_by_extension(EXTENSION)
         self.assertEqual(type(items), list, "should be list")
-        self.assertTrue(MTYPE in items[0]['MIME_type'])
+        self.assertTrue(MTYPE in items[0]["MIME_type"])
